e4daad86b8d64204dd63adeecc6c482a97ae1109
Alastor Bloode <73003829+AlastorTech@users.noreply.github.com>
2023-06-16 16:05:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pyupdater.client import Client
 from client_config import ClientConfig
 import traceback
 APP_NAME = "PyUpdater Tut"
-APP_VERSION = "1.0.0"
+APP_VERSION = "1.1.0"
 
 ASSET_NAME = "ffmpeg"
 ASSET_VERSION = '2.3.2'
